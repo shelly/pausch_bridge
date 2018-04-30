a89ad869ee430ed8180c1c55210430fd1b564949
Angela Qiu <angelaxqiu@gmail.com>
2018-04-29 21:53:53 -0400

    fade try 2

--- bridge_lights/demo.py
+++ bridge_lights/demo.py
@@ -46,27 +46,32 @@ def updateText(loc, text, c):
         idx = idx + 1
     time.sleep(0.5)
 
-    # # fade out
-    # idx = 5
-    # while(idx >= 0):
-    #     if (idx < len(s1) and s1[idx] != " "):
-    #         rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw((color[0] * idx * 0.2), (color[1] * idx * 0.2), (color[2] * idx * 0.2))
-    #         rig.updateOnce()
-    #         time.sleep(0.1)
-    #     if (idx < len(s2) and s2[idx] != " "):
-    #         rig.select("$side=bot[$panel=" + str(60-idx) + "]").setRGBRaw((color[0] * idx * 0.2), (color[1] * idx * 0.2), (color[2] * idx * 0.2))
-    #         rig.updateOnce()
-    #         time.sleep(0.1)
-    #     idx = idx - 1
-
-    # turn everything off
+    # fade out
     idx = 0
-    while (idx < 60):
-      rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw(0, 0, 0)
-      rig.select("$side=bot[$panel=" + str(60-idx) + "]").setRGBRaw(0, 0, 0)
-      idx = idx + 1
-    rig.updateOnce()
-    time.sleep(0.3)
+    idx2 = 5
+    while(idx2 >= 0):
+        while (idx < 60):
+            if (idx < len(s1) and s1[idx] != " "):
+                rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw((color[0] * idx * 0.2), (color[1] * idx * 0.2), (color[2] * idx * 0.2))
+                rig.updateOnce()
+                time.sleep(0.1)
+            if (idx < len(s2) and s2[idx] != " "):
+                rig.select("$side=bot[$panel=" + str(60-idx) + "]").setRGBRaw((color[0] * idx * 0.2), (color[1] * idx * 0.2), (color[2] * idx * 0.2))
+                rig.updateOnce()
+                time.sleep(0.1)
+            idx = idx + 1
+        rig.updateOnce()
+        time.sleep(0.1)
+        idx2 = idx2 - 1
+
+    # # turn everything off
+    # idx = 0
+    # while (idx < 60):
+    #   rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw(0, 0, 0)
+    #   rig.select("$side=bot[$panel=" + str(60-idx) + "]").setRGBRaw(0, 0, 0)
+    #   idx = idx + 1
+    # rig.updateOnce()
+    # time.sleep(0.3)
 
     # turn space spots white
     idx = 0
