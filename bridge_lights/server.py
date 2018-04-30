819504970f453fdf398bb7da4bd8c56a59449256
Shelly Bensal <shelly.bensal@gmail.com>
2018-04-29 21:06:06 -0400

    Adding bridge-side server code.

---
+++ bridge_lights/server.py
@@ -0,0 +1,9 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+def run(server_class=HTTPServer, handler_class=BaseHTTPRequestHandler):
+    server_address = ('', 8000)
+    httpd = server_class(server_address, handler_class)
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+	run()
--- bridge_webapp_site/bridge_webapp_site/forms.py
+++ bridge_webapp_site/bridge_webapp_site/forms.py
@@ -11,4 +11,25 @@ class TextForm(forms.Form):
 		self.your_text = self.cleaned_data['your_text']
 		self.your_color = self.cleaned_data['your_color']
 
-		requests.get('http://192.168.1.13/trigger/')
+		r, g, b = 252, 91, 123 
+
+		# if color == 'light_pink':
+		# 	r, g, b = 252, 91, 123
+		# elif color == 'orange':
+		# 	r, g, b = 252, 91, 123
+		# elif color == 'yellow':
+		# 	r, g, b = 252, 91, 123
+		# elif color == 'bright_pink':
+		# 	r, g, b = 252, 91, 123
+		# elif color == 'purple':
+		# 	r, g, b = 252, 91, 252 
+		# elif color == 'violet':
+		# 	r, g, b = 123, 91, 252
+		# elif color == 'blue':
+		# 	r, g, b = 41, 119, 255
+		# else: # teal 
+		# 	r, g, b = 62, 230, 242
+		url = 'http://192.168.1.13/trigger?var={0},{1},{2},{3}'.format(self.your_text, r, g, b)
+		print(url)
+		requests.get(url)
+
