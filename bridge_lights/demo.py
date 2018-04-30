fd1e934bc984eb562a5863e926f9bb57d91ff018
Angela Qiu <angelaxqiu@gmail.com>
2018-04-29 21:10:42 -0400

    ugh

--- bridge_lights/demo.py
+++ bridge_lights/demo.py
@@ -4,13 +4,14 @@ import time
 rig = L.Rig("/home/teacher/Lumiverse/PBridge.rig.json")
 rig.init()
 
-sent = "hi hi hi hi hi"
+sent = "hi hi hi hi hi hi hi hi hi hi"
 sent2 = "hello hello hello hello hello"
 color = (1, 0, 0)
 color2 = (0, 0, 1)
 
 # loc: top or bot
 def updateText(loc, text, c):
+    global sent, sent2, color, color2
     if not (loc == "top" or loc == "bottom"):
         print "not valid loc"
         return
@@ -42,31 +43,30 @@ def updateText(loc, text, c):
             rig.select("$side=bot[$panel=" + str(60-idx) + "]").setRGBRaw(0, 0, 1)
             rig.updateOnce()
             time.sleep(0.2)
-            idx = idx + 1
+        idx = idx + 1
     time.sleep(0.5)
 
-    # fade out
-    idx = 5
-    while(idx >= 0):
-        if (idx < len(s1) and s1[idx] != " "):
-            rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw((color[0] * idx * 0.2), (color[1] * idx * 0.2), (color[2] * idx * 0.2))
-            rig.updateOnce()
-            time.sleep(0.1)
-        if (idx < len(s2) and s2[idx] != " "):
-            rig.select("$side=bot[$panel=" + str(60-idx) + "]").setRGBRaw((color[0] * idx * 0.2), (color[1] * idx * 0.2), (color[2] * idx * 0.2))
-            rig.updateOnce()
-            time.sleep(0.1)
-        idx = idx - 1
-    # time.sleep(0.5)
+    # # fade out
+    # idx = 5
+    # while(idx >= 0):
+    #     if (idx < len(s1) and s1[idx] != " "):
+    #         rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw((color[0] * idx * 0.2), (color[1] * idx * 0.2), (color[2] * idx * 0.2))
+    #         rig.updateOnce()
+    #         time.sleep(0.1)
+    #     if (idx < len(s2) and s2[idx] != " "):
+    #         rig.select("$side=bot[$panel=" + str(60-idx) + "]").setRGBRaw((color[0] * idx * 0.2), (color[1] * idx * 0.2), (color[2] * idx * 0.2))
+    #         rig.updateOnce()
+    #         time.sleep(0.1)
+    #     idx = idx - 1
 
-    # # turn everything off
-    # idx = 0
-    # while (idx < 60):
-    #   rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw(0, 0, 0)
-    #   rig.select("$side=bot[$panel=" + str(60-idx) + "]").setRGBRaw(0, 0, 0)
-    #   idx = idx + 1
-    # rig.updateOnce()
-    # time.sleep(0.3)
+    # turn everything off
+    idx = 0
+    while (idx < 60):
+      rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw(0, 0, 0)
+      rig.select("$side=bot[$panel=" + str(60-idx) + "]").setRGBRaw(0, 0, 0)
+      idx = idx + 1
+    rig.updateOnce()
+    time.sleep(0.3)
 
     # turn space spots white
     idx = 0
