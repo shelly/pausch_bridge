f2a5d6afc451122030f40c41469a8856f6178dce
Angela Qiu <angelaxqiu@gmail.com>
2018-04-29 20:45:12 -0400

    fixing spacing

--- bridge_lights/demo.py
+++ bridge_lights/demo.py
@@ -11,47 +11,47 @@ color2 = (0, 0, 0)
 
 # loc: top or bot
 def updateText(loc, text, c):
-	if not (loc == "top" or loc == "bottom"):
-		print "not valid loc"
-		return
+    if not (loc == "top" or loc == "bottom"):
+        print "not valid loc"
+        return
 
-	if loc == "top":
-		sent = text
-		color = c
-	else:
-		sent2 = text
-		color2 = c
+    if loc == "top":
+        sent = text
+        color = c
+    else:
+        sent2 = text
+        color2 = c
 
-	sent = text + ((60 - len(text)) * " ")
-	sent2 = text2 + ((60 - len(text2)) * " ")
-	print ("sent is " + str(len(sent)) + ", sent2 is " + str(len(sent2)))
+    sent = text + ((60 - len(text)) * " ")
+    sent2 = text2 + ((60 - len(text2)) * " ")
+    print ("sent is " + str(len(sent)) + ", sent2 is " + str(len(sent2)))
 
 	# play animation of text from gates
-	idx = 0
-	while(idx < 60):
-		if (idx < len(sent) and sent[idx] != " "):
-			rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw(0.5, 0, 0)
-			rig.updateOnce()
-	       		time.sleep(0.1)
-		idx = idx + 1
+    idx = 0
+    while(idx < 60):
+        if (idx < len(sent) and sent[idx] != " "):
+            rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw(0.5, 0, 0)
+            rig.updateOnce()
+            time.sleep(0.1)
+        idx = idx + 1
 
 	# play animation of text from purnell
-	idx = 0
-	while(idx < 60):
-		if (idx < len(sent2) and sent2[idx] != " "):
-	        	rig.select("$side=bot[$panel=" + str(60-idx) + "]").setRGBRaw(0, 0, 1)
-	        	rig.updateOnce()
-	        	time.sleep(0.2)
-	        idx = idx + 1
-	time.sleep(0.5)
+    idx = 0
+    while(idx < 60):
+        if (idx < len(sent2) and sent2[idx] != " "):
+            rig.select("$side=bot[$panel=" + str(60-idx) + "]").setRGBRaw(0, 0, 1)
+            rig.updateOnce()
+            time.sleep(0.2)
+            idx = idx + 1
+    time.sleep(0.5)
 
 	# fade out
-	idx = 5
-	while(idx >= 0):
-		if (idx < len(sent) and sent[idx] != " "):
-			rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw((color[0] * idx * 0.2), (color[1] * idx * 0.2), (color[2] * idx * 0.2))
-			rig.updateOnce()
-	       	time.sleep(0.1)
+    idx = 5
+    while(idx >= 0):
+        if (idx < len(sent) and sent[idx] != " "):
+            rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw((color[0] * idx * 0.2), (color[1] * idx * 0.2), (color[2] * idx * 0.2))
+            rig.updateOnce()
+            time.sleep(0.1)
         if (idx < len(sent2) and sent2[idx] != " "):
             rig.select("$side=bot[$panel=" + str(60-idx) + "]").setRGBRaw((color[0] * idx * 0.2), (color[1] * idx * 0.2), (color[2] * idx * 0.2))
             rig.updateOnce()
@@ -69,19 +69,19 @@ def updateText(loc, text, c):
 	# time.sleep(0.3)
 
 	# turn space spots white
-	idx = 0
-	while (idx < 60):
-		if (len(sent) + len(sent2) < 60):
-			print "HERE"
-			idx2 = len(sent)
-			while (idx2 < 60 - len(sent2)):
-				rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw(1, 1, 1)
-	        	        rig.select("$side=bot[$panel=" + str(idx) + "]").setRGBRaw(1, 1, 1)
-		else:
-			if (sent[idx] == " " and sent2[59-idx] == " "):
-				rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw(1, 1, 1)
-		        	rig.select("$side=bot[$panel=" + str(idx) + "]").setRGBRaw(1, 1, 1)
+    idx = 0
+    while (idx < 60):
+        if (len(sent) + len(sent2) < 60):
+            print "HERE"
+            idx2 = len(sent)
+            while (idx2 < 60 - len(sent2)):
+                rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw(1, 1, 1)
+                rig.select("$side=bot[$panel=" + str(idx) + "]").setRGBRaw(1, 1, 1)
+        else:
+            if (sent[idx] == " " and sent2[59-idx] == " "):
+                rig.select("$side=top[$panel=" + str(idx) + "]").setRGBRaw(1, 1, 1)
+                rig.select("$side=bot[$panel=" + str(idx) + "]").setRGBRaw(1, 1, 1)
 		idx = idx + 1
 
-	rig.updateOnce()
-	time.sleep(0.2)
+    rig.updateOnce()
+    time.sleep(0.2)
